480fe98f3b181a278841edd160bd53dfb8c8a87c
chiranthanHY <chiranthanhy@gmail.com>
2025-08-25 19:47:46 +0530

    ws

---
+++ apps/web/app/room/[roomid]/page.tsx
@@ -0,0 +1,17 @@
+
+export default async function({
+    params
+}: {
+    params: {
+        slug: string
+    }
+}) {
+    // const slug = (await params).slug;
+    // const roomId = await getRoomId(slug);
+    
+    // return <ChatRoom id={roomId}></ChatRoom>
+    return <div>
+
+    </div>
+
+}
